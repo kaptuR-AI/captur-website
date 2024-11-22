fa07860e509b771a57479f2f57dbf6fb1ad6beab
Harmaton <njagiiharmaton@gmail.com>
2024-11-22 14:08:50 +0300

    Theme setup

---
+++ components/landing-page/hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='grid lg:grid-cols-2 sm:grid-col-1'>
+        Detect Deepfake using captur AI
+    </div>
+  )
+}
