f8ac8ba27eb989462380a5df971a4929cedb036b
regan-mu <regansomi@gmail.com>
2024-11-23 23:22:37 +0300

    Resolve

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import ExtraSection from "@/components/landing-page/extra-section";
 import Footer from "@/components/landing-page/footer";
 import Hero from "@/components/landing-page/hero";
 import { Navbar } from "@/components/landing-page/navbar";
+import StepsSection from "@/components/landing-page/steps";
 
 export default function Home() {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <ExtraSection />
-    <Footer />
+      <Navbar />
+      <Hero />
+      <ExtraSection />
+      <Footer />
     </>
   );
 }
