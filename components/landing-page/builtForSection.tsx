3c1f7932e58470ad25ddef32c206f209a42edc32
Harmaton <njagiiharmaton@gmail.com>
2024-11-26 15:31:49 +0300

    Added SVG elements

--- components/landing-page/builtForSection.tsx
+++ components/landing-page/builtForSection.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 const BuiltForSection = () => {
     return (
         <section className="py-10 md:w-3/4 md:mx-auto p-4">
             <h2 className="text-center font-extrabold lg:text-4xl sm:text-2xl mb-20">Designed for Africa. Built for the World.</h2>
+           
             <div className="flex gap-10 justify-center flex-wrap">
+          
                 <div className="w-96 h-80 border rounded-lg flex items-center justify-center flex-col gap-4 p-10">
                     <h3 className="font-bold text-xl text-center">Localized Language Detection</h3>
                     <p className="text-sm text-gray-400 text-center">Built to recognize and analyse African languages and accents.</p>
@@ -15,6 +18,7 @@ const BuiltForSection = () => {
                     <h3 className="font-bold text-xl text-center">Scalable for Any Business</h3>
                     <p className="text-sm text-gray-400 text-center">From startups to governments, our tools fit youe needs.</p>
                 </div>
+                <Image src="/Vector.svg" alt="Blue Lines" width={400} height={300} className=" absolute center-0 left-0 -z-10" />
             </div>
         </section>
     )
--- components/landing-page/testimonialSection.tsx
+++ components/landing-page/testimonialSection.tsx
@@ -1,35 +1,35 @@
 import { Testimonial } from "@/types";
 import Marquee from "react-fast-marquee";
 import TestimonialCard from "../cards/testimonialCard";
-
+import Image from "next/image";
 export const dummyTestimonials: Testimonial[] = [
     {
-      text: 'Captur has been a game-changer for our institution. The multi-model verification platform has helped us authenticate digital identities and prevent fraud effectively.',
-      imageUrl: '/testimonial1.jpg',
+      text: 'DeepFake has been a game-changer for our institution. The multi-model verification platform has helped us authenticate digital identities and prevent fraud effectively.',
+      imageUrl: '/testimonial-1.svg',
       name: 'John Doe',
       role: 'CEO, Example Bank',
     },
     {
-      text: 'I was impressed by the accuracy and reliability of Captur\'s synthetic media detection. It gives us confidence in the digital content we interact with.',
-      imageUrl: '/testimonial2.jpg',
+      text: 'I was impressed by the accuracy and reliability of DeepFake\'s synthetic media detection. It gives us confidence in the digital content we interact with.',
+      imageUrl: '/testimonial-2.svg',
       name: 'Jane Smith',
       role: 'CIO, Acme Corporation',
     },
     {
-      text: 'Captur has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
-      imageUrl: '/testimonial3.jpg',
+      text: 'DeepFake has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
+      imageUrl: '/testimonial-3.svg',
       name: 'Michael Johnson',
       role: 'COO, Fintech Startup',
     },
     {
-        text: 'Captur has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
-        imageUrl: '/testimonial3.jpg',
+        text: 'DeepFake has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
+        imageUrl: '/testimonial-3.svg',
         name: 'Michael ',
         role: 'COO, Fintech Startup',
       },
       {
-        text: 'Captur has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
-        imageUrl: '/testimonial3.jpg',
+        text: 'DeepFake has streamlined our identity verification process and reduced our fraud risk significantly. The platform is easy to integrate and provides valuable insights.',
+        imageUrl: '/testimonial-2.svg',
         name: 'Michael Brans',
         role: 'COO, Fintech Startup',
       },
@@ -39,7 +39,8 @@ const TestimonialSection = () => {
     return (
         <section className="p-4">
             <h2 className="text-center font-extrabold mb-4 lg:text-4xl sm:text-2xl">Voices of Urgency</h2>
-            <Marquee >
+            <Image src="/Vector.svg" alt="Blue Lines" width={400} height={300} className="absolute -top-50 right-0 -z-10" />
+            <Marquee pauseOnHover pauseOnClick >
             {dummyTestimonials.map((testimonial) => (
           <TestimonialCard key={testimonial.name} testimonial={testimonial} />
         ))}
