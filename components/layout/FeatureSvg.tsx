1340ff10d0fd3981b924a3a22d7b7552c5b61cf2
samuelsenerwa <samuelsenerwa@gmail.com>
2024-12-12 11:05:10 +0300

    DEEPTRACK-2/feat: styled feature component to be responsive in mobile

---
+++ components/layout/FeatureSvg.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+interface FeatureSvgProps {
+    imageSrc: string;
+    altText: string;
+  }
+  
+const FeatureSvg = ({ imageSrc, altText }: FeatureSvgProps) => {
+    return (
+      <div className='relative mt-6'>
+          <Image data-aos='fade-in' src={imageSrc} className='w-full m-auto' height={100} width={100} alt={altText} />
+      </div>
+    )
+  }
+
+  export default FeatureSvg;
