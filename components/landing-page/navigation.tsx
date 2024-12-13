2542057fb7380fa9072cc80d843b31bf6379866f
samuelsenerwa <samuelsenerwa@gmail.com>
2024-12-13 16:10:06 +0300

    DEEPTRACK-2/fix:fixed the overlapping header content

--- components/landing-page/navigation.tsx
+++ components/landing-page/navigation.tsx
@@ -106,7 +106,23 @@ export function Navigation() {
         <NavigationMenuItem>
           <NavigationMenuTrigger className="bg-black text-white ">Use Cases</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr] ">
+              <li className="row-span-4">
+                <NavigationMenuLink asChild>
+                  <Link
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      <Image src={'/logos/deeptrack-high-resolution-logo-black-transparent.png'} width={200} height={50} alt="Logo" />
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Use AI to detect deepfakes. DeepTrack is model agnostic
+                    </p>
+                  </Link>
+                </NavigationMenuLink>
+              </li>
               {components.map((component) => (
                 <ListItem
                   key={component.title}
